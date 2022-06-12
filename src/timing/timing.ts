25864c6345fb39434e8626c772051b7cd0666ef6
Hanifi YILDIRIMDAGI <h.yildirimdagi@amatis.nl>
2022-06-12 14:23:30 +0300

    CancellationWork added

--- src/timing/timing.ts
+++ src/timing/timing.ts
@@ -3,12 +3,23 @@ export default class Timing {
   /**
    * @param ms
    * @example await Wait(1000) | await Wait(TimeSpan.FromSeconds(1))
-   * @example 
+   * @example
    * doSomething...
    * await Wait(TimeSpan.FromMinute(1))
    * continue...
    */
   public static async Sleep(ms: number): Promise<unknown> {
-    return new Promise((resolve) =>  setTimeout(resolve, ms));
+    return new Promise((resolve) => setTimeout(resolve, ms));
   }
+
+  //TODO?: Add a summary
+  public static CancellationWork = (function () {
+    let timer: NodeJS.Timeout;
+    return function (callback:CancellationWorkCallback, ms:number) {
+      clearTimeout(timer);
+      timer = setTimeout(callback, ms);
+    };
+  })();
 }
+
+type CancellationWorkCallback = () =>void;
--- tests/timing-timing.spec.ts
+++ tests/timing-timing.spec.ts
@@ -1,4 +1,4 @@
-import { assert } from "chai";
+import { assert, expect } from "chai";
 import "mocha";
 import TimeSpan from "../src/timing/time-span";
 import Timing from "../src/timing/timing";
@@ -11,5 +11,15 @@ describe("Timing | Timing", () => {
     const elaptedTime = Date.now() - start;
     const result = elaptedTime >= span && elaptedTime <= span * 1.1;
     assert.isTrue(result);
+  });
+  it("Workflow | CancellationWork", async () => {
+    let finalResponse: number = 0;
+    for (let i = 0; i < 5; i++) {
+      Timing.CancellationWork(() => {
+        finalResponse += 10;
+      }, 50);
+    }
+    await new Promise((resolve) => setTimeout(resolve,50));
+    assert.equal(finalResponse,10);
   });
 });
