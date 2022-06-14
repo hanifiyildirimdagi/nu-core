13fc6d99289ffab7da8d2ac86c8b5927856b1ca5
Hanifi YILDIRIMDAGI <h.yildirimdagi@amatis.nl>
2022-06-14 10:24:24 +0300

    CancellationWork summary added

--- src/timing/timing.ts
+++ src/timing/timing.ts
@@ -12,14 +12,37 @@ export default class Timing {
     return new Promise((resolve) => setTimeout(resolve, ms));
   }
 
-  //TODO?: Add a summary
+  /**
+   * ## CancellationWork
+   * This static method allows a job to be done only once, within a period of time you specify. 
+   * For example, you produce a console output every time a character is entered into the input box. 
+   * But you want to produce results when the user is no longer entering data for a second. 
+   * You can use `CancellationWork` for this and similar operations.
+   * @static
+   * @param callback : CancellationWorkCallback
+   * @example
+   * > Example
+   * >
+   * > import { Timing } from "necessary-utils-core";
+   * >
+   * > const work = () => {
+   * >   console.log("Hello.");
+   * > };
+   * > Timing.CancellationWork(work, 1000);
+   * > Timing.CancellationWork(work, 1000);
+   * > Timing.CancellationWork(work, 1000);
+   * > Timing.CancellationWork(work, 1000);
+   * > Timing.CancellationWork(work, 1000);
+   * > // Output : Hello. One time.
+   * >
+   */
   public static CancellationWork = (function () {
     let timer: NodeJS.Timeout;
-    return function (callback:CancellationWorkCallback, ms:number) {
+    return function (callback: CancellationWorkCallback, ms: number) {
       clearTimeout(timer);
       timer = setTimeout(callback, ms);
     };
   })();
 }
 
-type CancellationWorkCallback = () =>void;
+type CancellationWorkCallback = () => void;
